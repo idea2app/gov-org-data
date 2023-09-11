f72009c42e3074782381aebda9b6927589b17e9c
TechQuery <shiy2008@gmail.com>
2023-09-11 22:02:24 +0800

    [refactor] simplify Source codes with latest JS syntax & API [migrate] replace Yarn with PNPM to reduce Disk usage

--- index.js
+++ index.js
@@ -1,69 +1,50 @@
-var Crawler = require("crawler");
+const Crawler = require('crawler');
 const cheerio = require('cheerio');
-const fs = require('fs');
-const c = new Crawler({
-    maxConnections: 10
-});
+const { promises } = require('fs');
+
+const crawler = new Crawler({ maxConnections: 10 });
 
 // 所有爬虫链接
 const urls = [
-    'https://sc.huatu.com/syzwb/2021/1/buweisearch/1.html', 
+    'https://sc.huatu.com/syzwb/2021/1/buweisearch/1.html',
     'https://sc.huatu.com/syzwb/2021/8/buweisearch/1.html',
     'https://sc.huatu.com/syzwb/2022/2/buweisearch/1.html',
 ];
 
-let promiseList = [];
-urls.forEach(v => {
-    promiseList.push(spiderItem(v));
-})
+const promiseList = urls.map(async (uri) => {
+    var finish;
 
-Promise.all(promiseList).then(ress => {
+    const { body } = await new Promise((resolve, reject) =>
+        crawler.queue([
+            {
+                uri,
+                jQuery: true,
+                callback(error, res, done) {
+                    finish = done;
+                    error ? reject(error) : resolve(res);
+                },
+            },
+        ])
+    );
+    const $ = cheerio.load(body);
 
-    var datas = ress.reduce(function (a, b) { return a.concat(b) });
-    datas = Array.from(new Set(datas))
+    finish();
 
-    fs.writeFile(__dirname + '/data.json', JSON.stringify(
-        datas
-    ), function (err) {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log(datas.length + "条记录，成功写入完成")
-        }
-    })
-})
+    return [...$('li h4')]
+        .map((item) => {
+            const orgName = $(item)
+                .text()
+                .replace(/（.+?）/g, '');
 
-function spiderItem(url) {
-    return new Promise((resolve, reject) => {
-        c.queue([{
-            uri: url,
-            jQuery: true,
-            callback: function (error, res, done) {
-                if (error) {
-                    reject(error);
-                } else {
-                    const $ = cheerio.load(res.body)
-                    const datas = []
-                    $('li h4').each(function () {
-                        let orgName = $(this).text();
-                        orgName = orgName.replace(/(\（[^\)]*\）)/,'')
-                        if(orgName.indexOf('所属事业单位') != -1){
-                           return;
-                        }
-                        if(!(/^([^、，])*$/.test(orgName))){
-                            const names = (orgName.split('、').length > 1) ? orgName.split('、') : orgName.split('，');
-                            for(i in names){
-                                orgName = names[i];
-                            }
-                        }
-                        orgName = orgName.replace(/(各)[0-9](人|名)/,'')
-                        orgName = orgName.replace(/[0-9](人|名)/,'')
-                        datas.push(orgName);
-                    })
-                    resolve(datas)
-                }
-                done();
-            }
-        }]);
-    })
-}
+            return !orgName.includes('所属事业单位') && orgName.split(/、|，/);
+        })
+        .filter(Boolean);
+});
+
+Promise.all(promiseList).then(async (list) => {
+    var data = [...new Set(list.flat(Infinity))];
+
+    await promises.writeFile('data.json', JSON.stringify(data));
+
+    console.log(data.length + '条记录，成功写入完成');
+});
