0d70acde00e08fd22e840a15973f7475ab08a0c2
TechQuery <shiy2008@gmail.com>
2025-10-30 05:14:49 +0800

    [add] Target List Model for Local File System based on MobX-RESTful 2.x [optimize] replace Promise.all() with Async Generator syntax [migrate] upgrade to JSDOM 27, PNPM 10, TSX 4, Lint-Staged 16, Husky & other latest Upstream packages

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import 'array-unique-proposal';
-import { promises } from 'fs';
+import { outputFile } from 'fs-extra';
 import { JSDOM } from 'jsdom';
 import { parse } from 'path';
 import { stringify } from 'yaml';
@@ -28,55 +28,46 @@ interface Job extends Record<'positions' | 'vacancies' | 'applicants', number> {
     name: string;
 }
 
-const promiseList = urls.map(async (uri) => {
-    const {
-        window: { document },
-    } = await JSDOM.fromURL(uri);
+async function* loadList(links: string[]): AsyncGenerator<Job> {
+    for (const link of links) {
+        const {
+            window: { document },
+        } = await JSDOM.fromURL(link);
 
-    return [...document.querySelectorAll('.searchList > li > a')]
-        .map(({ children }) => {
-            const [name, counts] = [...children];
+        for (const { children } of document.querySelectorAll('.searchList > li > a')) {
+            const [name, counts] = children;
 
             const orgName = name.textContent?.trim().replace(/（.+?）/g, '');
             const [positions, vacancies, applicants] =
                 counts.textContent?.match(/\d+/g)?.map(Number) || [];
 
-            return (
-                !orgName?.includes('所属事业单位') && {
-                    name: orgName,
-                    positions,
-                    vacancies,
-                    applicants,
-                }
-            );
-        })
-        .filter(Boolean) as Job[];
-});
+            if (!orgName?.includes('所属事业单位'))
+                yield { name: orgName, positions, vacancies, applicants };
+        }
+    }
+}
+const list = await Array.fromAsync(loadList(urls as string[]));
 
-const list = await Promise.all(promiseList);
-
-const data = (list.flat(Infinity) as Job[]).uniqueBy('name');
+const data = list.uniqueBy('name');
 
 const { ext } = parse(output);
+
+const stringifyCSV = (data: object[]) =>
+    [
+        Object.keys(data[0]) + '',
+        ...data.map((item) => Object.values(item).map((value) => JSON.stringify(value)) + ''),
+    ].join('\n');
 
 const text =
     ext === '.json'
         ? JSON.stringify(data, null, 4)
         : /^\.ya?ml$/.test(ext)
-        ? stringify(data)
-        : ext === '.csv'
-        ? [
-              Object.keys(data[0]) + '',
-              ...data.map(
-                  (item) =>
-                      Object.values(item).map((value) =>
-                          JSON.stringify(value),
-                      ) + '',
-              ),
-          ].join('\n')
-        : '';
+          ? stringify(data)
+          : ext === '.csv'
+            ? stringifyCSV(data)
+            : '';
 
-await promises.writeFile(output, text);
+await outputFile(output, text);
 
 console.log(data.length + '条记录，成功写入完成');
 console.timeEnd(output);
